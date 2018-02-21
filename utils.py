2a7d63d97fa06715cbd2bd0a64b7d1da3a34eb7c
esuji <esuji@beproud.jp>
2018-02-22 00:20:17 +0900

    some fixes

--- utils.py
+++ utils.py
@@ -9,7 +9,7 @@ def get_step1_inputer_dict(db):
     select_sql = 'SELECT step1_inputer, COUNT(step1_inputer) FROM yuyu_data GROUP BY step1_inputer'
     cur = db.engine.execute(select_sql)
     data = cur.fetchall()
-    step1_inputer_dict = {r['step1_inputer']: r['count(step1_inputer)'] for r in data}
+    step1_inputer_dict = {name: count for name, count in data}
     return step1_inputer_dict
 
 # select step, count(step) from yuyu_data
