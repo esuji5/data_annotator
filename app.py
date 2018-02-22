f42164bafb17f1dde50a0505b7002ec52fccbd33
esuji <esuji@beproud.jp>
2018-02-22 11:16:26 +0900

    サーバー側をリファクタリング

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
-import random
-
 from flask_sqlalchemy import SQLAlchemy
-from flask import Flask, request, redirect, session, g, render_template, flash, url_for
+from flask import Flask, request, redirect, session, render_template, url_for
 from flask_s3 import FlaskS3
-# from bokeh.charts import Histogram
-# from bokeh.embed import components
-# from bokeh.plotting import figure
 
-from utils import get_image_data
-from utils import get_step1_inputer_dict
+from basic_auth import requires_auth
+from data import get_image_data
+from data import get_step1_inputer_dict
+from data import fetch_next_rand_id
+from data import update_step1
 
 # setup flask
 app = Flask(__name__)
@@ -18,6 +16,52 @@ if not app.config['DEBUG'] and not app.config['TESTING']:
     s3 = FlaskS3(app)
 db = SQLAlchemy(app)
 
+
+def get_avatar():
+    return session.get('avatar', None)
+
+
+@app.route('/')
+@requires_auth
+def top():
+    step1_inputer_dict = get_step1_inputer_dict(db)
+    next_rand_id = fetch_next_rand_id(db)
+    return render_template('index.html', step1_inputer_dict=step1_inputer_dict,
+                           next_rand_id=next_rand_id, avatar=get_avatar())
+
+
+@app.route('/set_avatar/<avatar>')
+@requires_auth
+def set_avatar(avatar):
+    if avatar == 'none':
+        session['avatar'] = None
+    else:
+        session['avatar'] = avatar
+    return redirect(url_for('top'))
+
+
+@app.route('/annotate/<koma_id>')
+@requires_auth
+def annotate(koma_id):
+    img_data = get_image_data(db, koma_id)
+    img_path = img_data['img_path'].split('yuyu_data/')[-1]
+    next_rand_id = fetch_next_rand_id(db)
+    return render_template('annotate.html', img_path=img_path, img_data=img_data,
+                           koma_id=koma_id, next_rand_id=next_rand_id, avatar=get_avatar())
+
+
+@app.route('/save_annotate', methods=['POST'])
+@requires_auth
+def save_annotate():
+    # request.formのMultiDictだと変なlist型になるので普通のdictにする
+    val_dict = {key: val for key, val in request.form.items()}
+    update_step1(db, val_dict)
+    next_rand_id = fetch_next_rand_id(db)
+    return redirect('/annotate/' + next_rand_id)
+
+
+if __name__ == '__main__':
+    app.run()
 
 # @app.before_request
 # def before_request():
@@ -34,84 +78,9 @@ db = SQLAlchemy(app)
 #     """Closes the database again at the end of the request."""
 #     if hasattr(g, 'sqlite_db'):
 #         g.sqlite_db.close()
-
-def get_avatar():
-    return session.get('avatar', None)
-
-
-def fetch_next_rand_id():
-    # db = get_db(app)
-    step1_ids_sql = 'SELECT koma_id FROM yuyu_data WHERE step = 1'
-    step1_ids = db.engine.execute(step1_ids_sql).fetchall()
-    next_rand_id = random.choice(step1_ids)['koma_id']
-    return next_rand_id
-
-
-@app.route('/')
-def top():
-    step1_inputer_dict = get_step1_inputer_dict(db)
-    next_rand_id = fetch_next_rand_id()
-    return render_template('index.html', step1_inputer_dict=step1_inputer_dict,
-                           next_rand_id=next_rand_id, avatar=get_avatar(),
-                           )
-
-
-@app.route('/set_avatar/<avatar>')
-def set_avatar(avatar):
-    if avatar == 'none':
-        session['avatar'] = None
-    else:
-        session['avatar'] = avatar
-    return redirect(url_for('top'))
-
-
-@app.route('/annotate/<koma_id>')
-def annotate(koma_id):
-    img_data = get_image_data(db, koma_id)
-    img_path = url_for('static', filename=img_data['img_path'].split('yuyu_data/')[-1])
-    img_path = img_data['img_path'].split('yuyu_data/')[-1]
-    print(img_path)
-    print(url_for('static', filename='style.css'))
-    next_rand_id = fetch_next_rand_id()
-    return render_template('annotate.html', img_path=img_path, img_data=img_data,
-                           koma_id=koma_id, next_rand_id=next_rand_id, avatar=get_avatar())
-
-
-@app.route('/save_annotate', methods=['POST'])
-def save_annotate():
-    # request.formのMultiDictだと変なlist型になるので普通のdictにする
-    val_dict = {key: val for key, val in request.form.items()}
-
-    update_sql = """
-    UPDATE yuyu_data SET
-    chara_num = {chara_num},
-    whos = '{whos}',
-    face_direction = '{face_direction}',
-    eyes = '{eyes}',
-    step1_inputer = '{step1_inputer}',
-    step = 2
-    where koma_id = '{koma_id}'
-    """.format(**val_dict)
-
-    # db = get_db(app)
-    db.engine.execute(update_sql)
-    # db.engine.commit()
-    flash('update was successfully posted')
-    next_rand_id = fetch_next_rand_id()
-    return redirect('/annotate/' + next_rand_id)
-
-
-if __name__ == '__main__':
-    app.run()
-
-
-# データを集めるカラム
-columns = ['koma_id', 'img_path', 'kanji', 'page', 'position', 'koma', 'size_x', 'size_y',
-           'chara_num', 'whos', 'eyes', 'face_direction',  # 1次入力
-           'place', 'background', 'serif_num', 'whos_s',  # 2次入力
-           'serifs', 'onomatope',  # 3次入力
-           'step', 'step1_inputer', 'step2_inputer', 'step3_inputer', ]  # 入力ステップ管理
-
+# from bokeh.charts import Histogram
+# from bokeh.embed import components
+# from bokeh.plotting import figure
 # import math
 # from collections import namedtuple
 
---
+++ data.py
@@ -0,0 +1,57 @@
+import random
+
+from flask import flash
+
+
+def get_image_data(db, koma_id):
+    select_sql = "SELECT img_path FROM yuyu_data WHERE koma_id = '{}'".format(koma_id)
+    cur = db.engine.execute(select_sql)
+    data = cur.fetchone()
+    return data
+
+
+def get_step1_inputer_dict(db):
+    select_sql = 'SELECT step1_inputer, COUNT(step1_inputer) FROM yuyu_data GROUP BY step1_inputer'
+    cur = db.engine.execute(select_sql)
+    data = cur.fetchall()
+    step1_inputer_dict = {name: count for name, count in data}
+    return step1_inputer_dict
+
+
+def fetch_next_rand_id(db):
+    # db = get_db(app)
+    step1_ids_sql = 'SELECT koma_id FROM yuyu_data WHERE step = 1'
+    step1_ids = db.engine.execute(step1_ids_sql).fetchall()
+    next_rand_id = random.choice(step1_ids)['koma_id']
+    return next_rand_id
+
+
+def update_step1(db, val_dict):
+    update_sql = """
+    UPDATE yuyu_data SET
+    chara_num = {chara_num},
+    whos = '{whos}',
+    face_direction = '{face_direction}',
+    eyes = '{eyes}',
+    step1_inputer = '{step1_inputer}',
+    step = 2
+    where koma_id = '{koma_id}'
+    """.format(**val_dict)
+
+    # db = get_db(app)
+    db.engine.execute(update_sql)
+    # db.engine.commit()
+    flash('update was successfully posted')
+
+
+# データを集めるカラム
+columns = ['koma_id', 'img_path', 'kanji', 'page', 'position', 'koma', 'size_x', 'size_y',
+           'chara_num', 'whos', 'eyes', 'face_direction',  # 1次入力
+           'place', 'background', 'serif_num', 'whos_s',  # 2次入力
+           'serifs', 'onomatope',  # 3次入力
+           'step', 'step1_inputer', 'step2_inputer', 'step3_inputer', ]  # 入力ステップ管理
+
+# select step, count(step) from yuyu_data
+# group by step
+# select step1_inputer, count(step1_inputer) from yuyu_data
+# group by step1_inputer
