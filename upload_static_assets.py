3d5300da9767b4723e179ec5af889e51fd7e7ea0
esuji <esuji5@gmail.com>
2018-06-28 13:23:09 +0900

    Update upload_static_assets.py

--- upload_static_assets.py
+++ upload_static_assets.py
@@ -1,8 +1,8 @@
 import flask_s3
 from app import app
 
-app.config['AWS_ACCESS_KEY_ID'] = 'AKIAIHGYOSPAJA4TQM5A'
-app.config['AWS_SECRET_ACCESS_KEY'] = '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'
+app.config['AWS_ACCESS_KEY_ID'] = '{AWS_ACCESS_KEY_ID}'
+app.config['AWS_SECRET_ACCESS_KEY'] = '{AWS_SECRET_ACCESS_KEY}'
 app.config['FLASKS3_BUCKET_NAME'] = 'yuyudata'
 
 flask_s3.create_all(app, filepath_filter_regex=r'(style.css|index.js)')
